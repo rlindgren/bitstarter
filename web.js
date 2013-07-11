f444198babd538332e54652ac0db44b6431778ec
Ryan Lindgren <ryan.lindgren@gmail.com>
2013-07-11 03:10:34 +0000

    modify web.js to send a buffer read from file system

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var index = new Buffer(fs.readFileSync(__dirname + '/index.html'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.pipe(index);
 });
 
 var port = process.env.PORT || 5000;
